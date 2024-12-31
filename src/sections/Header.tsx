29a8cfca4a72236da6e464f21531592be9e0a6df
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2024-12-31 15:51:03 +0530

    style(Header.tsx): improve header layout and styling for better user experience and visual appeal

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,9 +1,37 @@
 import ArrowRight from "../assets/arrow-right.svg";
+import Logo from "@/assets/logosaas.png";
+import Image from "next/image";
+import MenuIcon from "@/assets/menu.svg";
 export const Header = () => {
   return (
-    <div className="flex justify-center items-center py-3 bg-black text-white text-sm">
-      <p> Get Started for free</p>
-      <ArrowRight className="w-4 h-4 inline-flex justify-center items-center" />
-    </div>
+    <header className="sticky top-0">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
+        <p className="text-white/60 hidden md:block">
+          Streamline your workflow and boost your productivity
+        </p>
+        <div className="inline-flex gap-1 items-center">
+          <p> Get Started for free</p>
+          <ArrowRight className="w-4 h-4 inline-flex justify-center items-center" />
+        </div>
+      </div>
+      <div className="py-5">
+        <div className="container">
+          <div className="flex items-center justify-between">
+            <Image src={Logo} alt="Logo" height={40} width={40} />
+            <MenuIcon className="w-5 h-5 md:hidden" />
+            <nav className="hidden md:flex gap-6 text-black/60 items-center">
+              <a href="#">About</a>
+              <a href="#">Features</a>
+              <a href="#">Customers</a>
+              <a href="#">Updates</a>
+              <a href="#">help</a>
+              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex justify-center align-middle tracking-tight">
+                Get for free
+              </button>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
   );
 };
