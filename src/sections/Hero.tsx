657f683fe61998c4a00febec0c30bafb55e6832e
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-01-02 09:12:05 +0530

    feat(page.tsx): add Hero component to the Home page for improved user experience feat(Hero.tsx): implement Hero component with content to showcase app features and encourage user engagement

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,17 @@
 export const Hero = () => {
-  return null;
+  return (
+    <section>
+      <div className="container">
+        <div></div>
+        <h1>Pathway to Productivity</h1>
+        <p>
+          Celebrate the joy of accomplishment with an app designed to your
+          progress, motivate your efforts, and celebrate your successes.
+        </p>
+        <div>
+          <button>Get Started</button>
+        </div>
+      </div>
+    </section>
+  );
 };
