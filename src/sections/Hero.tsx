5c8f75f4a2897de8b6957704318799f92afc0ce8
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-01-02 17:31:29 +0530

    style(globals.css): add new styles for buttons to improve consistency and readability style(Header.tsx): add backdrop-blur-sm class to header for a visual effect style(Hero.tsx): update Hero section styles for improved layout and design style(tsconfig.json): reformat compilerOptions in tsconfig.json for better readability and maintainability

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,15 +1,34 @@
+import ArrowIcon from "@/assets/arrow-right.svg";
+import cogImage from "@/assets/cog.png";
+import Image from "next/image";
 export const Hero = () => {
   return (
-    <section>
+    <section className="pt-8 pb-20 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_66%)]">
       <div className="container">
-        <div></div>
-        <h1>Pathway to Productivity</h1>
-        <p>
-          Celebrate the joy of accomplishment with an app designed to your
-          progress, motivate your efforts, and celebrate your successes.
-        </p>
-        <div>
-          <button>Get Started</button>
+        <div className="md:flex">
+          <div className="md:w-[478px]">
+            <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
+              Version 2.0 is here
+            </div>
+            <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
+              Pathway to Productivity
+            </h1>
+            <p className="text-xl text-[#010D3E] tracking-tight mt-6">
+              Celebrate the joy of accomplishment with an app designed to your
+              progress, motivate your efforts, and celebrate your successes.
+            </p>
+            <div className="flex gap-1 items-center mt-[30px]">
+              <button className="btn btn-primary">Get for free</button>
+              <button className="btn btn-text gap-1">
+                <span>Learn more</span>
+                <ArrowIcon className="h-5 w-5" />
+              </button>
+            </div>
+          </div>
+
+          <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1">
+            <Image src={cogImage} alt="cog" />
+          </div>
         </div>
       </div>
     </section>
