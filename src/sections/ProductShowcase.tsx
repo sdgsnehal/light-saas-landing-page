dd7b144dd7c4b83829714613b9a8c38744b99baa
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-01-03 17:33:06 +0530

    style(globals.css): add new CSS class '.tag' for consistent styling of tags feat(page.tsx): add ProductShowcase component to the Home page for showcasing products style(Hero.tsx): replace inline CSS with '.tag' class for consistent styling of tags feat(ProductShowcase.tsx): add ProductShowcase component with product image and description for showcasing products

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,3 +1,22 @@
+import productImage from "@/assets/product-image.png";
+import Image from "next/image";
 export const ProductShowcase = () => {
-  return null;
+  return (
+    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24">
+      <div className="container">
+        <div className="flex justify-center">
+          <div className="tag">Boost Your Productivity</div>
+        </div>
+
+        <h2 className="text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
+          A more effective way to track progress
+        </h2>
+        <p className="text-center lea">
+          Effortlessly turn your idea into a fully functional, responsive, SaaS
+          website in just minutes with this template
+        </p>
+        <Image src={productImage} alt="product image" />
+      </div>
+    </section>
+  );
 };
