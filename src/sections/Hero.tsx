0120f915c38d3edf5b3483ad00f2568d5c3a4068
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-01-03 12:37:32 +0530

    feat(Hero.tsx): add cylinderImage to display a new image in the Hero section style(Hero.tsx): adjust styling for better responsiveness and layout in the Hero section

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,16 +1,17 @@
 import ArrowIcon from "@/assets/arrow-right.svg";
 import cogImage from "@/assets/cog.png";
+import cylinderImage from "@/assets/cylinder.png";
 import Image from "next/image";
 export const Hero = () => {
   return (
-    <section className="pt-8 pb-20 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_66%)]">
+    <section className="pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_66%)]">
       <div className="container">
-        <div className="md:flex">
+        <div className="md:flex items-center">
           <div className="md:w-[478px]">
             <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">
               Version 2.0 is here
             </div>
-            <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
+            <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">
               Pathway to Productivity
             </h1>
             <p className="text-xl text-[#010D3E] tracking-tight mt-6">
@@ -26,8 +27,20 @@ export const Hero = () => {
             </div>
           </div>
 
-          <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1">
-            <Image src={cogImage} alt="cog" />
+          <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 relative">
+            <Image
+              src={cogImage}
+              alt="cog"
+              className="md:absolute md:h-full md:w-auto md:max-w-none md:-left-6"
+            />
+            <Image
+              src={cylinderImage}
+              alt="cylinder"
+              
+              width={220}
+              height={220}  
+              className="hidden md:block -top-8 -left-32 md:absolute"
+            />
           </div>
         </div>
       </div>
