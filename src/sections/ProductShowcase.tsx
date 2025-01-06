9dccfbbcba8ad04262c10e082d010f5a8abe3723
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-01-06 11:40:03 +0530

    style(globals.css): fix formatting issue in .tag class by adding missing semicolon feat(ProductShowcase.tsx): improve styling and responsiveness of ProductShowcase section by adjusting text sizes, line heights, and image placements

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,21 +1,42 @@
 import productImage from "@/assets/product-image.png";
 import Image from "next/image";
+import pyramidImage from "@/assets/pyramid.png";
+import tubeImage from "@/assets/tube.png";
 export const ProductShowcase = () => {
   return (
-    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24">
+    <section className="bg-gradient-to-b from-[#FFFFFF] to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
-        <div className="flex justify-center">
-          <div className="tag">Boost Your Productivity</div>
-        </div>
+        <div className="max-w-[540px] mx-auto">
+          <div className="flex justify-center">
+            <div className="tag">Boost Your Productivity</div>
+          </div>
 
-        <h2 className="text-center text-3xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
-          A more effective way to track progress
-        </h2>
-        <p className="text-center lea">
-          Effortlessly turn your idea into a fully functional, responsive, SaaS
-          website in just minutes with this template
-        </p>
-        <Image src={productImage} alt="product image" />
+          <h2 className="text-center text-3xl md:text-[54px] md:leading-[60px] font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-5">
+            A more effective way to track progress
+          </h2>
+          <p className="text-center text-[22px] leading-[30px] tracking-tight text-[#010D3E] mt-5">
+            Effortlessly turn your idea into a fully functional, responsive,
+            no-code Saas website in just minutes with the set of free components
+            for Framer.
+          </p>
+        </div>
+        <div className="relative">
+          <Image src={productImage} alt="product image" className="mt-10" />
+          <Image
+            src={pyramidImage}
+            alt="pyramid image"
+            height={262}
+            width={262}
+            className="hidden md:block absolute -right-36 -top-32"
+          />
+          <Image
+            src={tubeImage}
+            alt="tube image"
+            height={248}
+            width={248}
+            className="hidden md:block absolute bottom-24 -left-36"
+          />
+        </div>
       </div>
     </section>
   );
