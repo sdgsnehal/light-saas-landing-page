9dfa689312439992ccdfa940427d0592e5c031a1
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-01-06 16:49:34 +0530

    feat(Pricing.tsx): add CheckIcon component and improve styling for pricing tiers section to enhance visual appeal and user experience

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,3 +1,5 @@
+import CheckIcon from "@/assets/check.svg";
+import { twMerge } from "tailwind-merge";
 const pricingTiers = [
   {
     title: "Free",
@@ -59,6 +61,78 @@ export const Pricing = () => {
           Free forever upgrade for Unlimited tasks, better security and
           exclusive features.
         </p>
+        <div>
+          {pricingTiers.map(
+            ({
+              title,
+              monthlyPrice,
+              buttonText,
+              popular,
+              inverse,
+              features,
+            }) => (
+              <div
+                key={title}
+                className={twMerge(
+                  "p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA]",
+                  inverse === true && "border-black bg-black text-white/60"
+                )}
+              >
+                <div className="flex justify-between">
+                  <h3
+                    className={twMerge(
+                      "text-lg font-bold text-black/50",
+                      inverse === true && "text-white/60"
+                    )}
+                  >
+                    {title}
+                  </h3>
+                  {popular === true && (
+                    <div className="inline-flex text-sm px-4 py-1.5 rounded-xl  border border-white/20">
+                      <span
+                        className={twMerge(
+                          "bg-linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#3BFFFF,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium",
+                          inverse === true && "text-white/60"
+                        )}
+                      >
+                        Popular
+                      </span>
+                    </div>
+                  )}
+                </div>
+
+                <div className="flex items-baseline gap-1 mt-[30px]">
+                  <span className="text-4xl font-bold tracking-tighter leading-none">
+                    {monthlyPrice}
+                  </span>
+                  <span className="tracking-tight font-bold text-black/50">
+                    /month
+                  </span>
+                </div>
+                <button
+                  className={twMerge(
+                    "btn btn-primary w-full mt-[30px]",
+                    inverse === true && "bg-white text-black"
+                  )}
+                >
+                  {buttonText}
+                </button>
+                <ul className="flex flex-col gap-5 scroll-mt-8">
+                  {features.map((feature) => (
+                    <li
+                      key={feature}
+                      className="text-sm flex items-center gap-4"
+                    >
+                      <CheckIcon className="w-6 h-6" />
+                      <span>{feature}</span>
+                    </li>
+                  ))}
+                  .
+                </ul>
+              </div>
+            )
+          )}
+        </div>
       </div>
     </section>
   );
