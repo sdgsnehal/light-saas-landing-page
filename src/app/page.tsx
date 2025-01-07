44aa557551f99c29da4ce2231e6408a33d15f738
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-01-07 12:47:10 +0530

    feat(page.tsx): add CallToAction section to Home page for improved user engagement and conversion feat(CallToAction.tsx): implement CallToAction section with sign up form and motivational content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Hero } from "../sections/Hero";
 import { ProductShowcase } from "@/sections/ProductShowcase";
 import { Pricing } from "@/sections/Pricing";
 import { Testimonials } from "@/sections/Testimonials";
+import { CallToAction } from "@/sections/CallToAction";
 export default function Home() {
   return (
     <>
@@ -12,7 +13,8 @@ export default function Home() {
       <LogoTicker />
       <ProductShowcase />
       <Pricing />
-      <Testimonials/>
+      <Testimonials />
+      <CallToAction />
     </>
   );
 }
--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,3 +1,21 @@
+import ArrowRight from "@/assets/arrow-right.svg";
 export const CallToAction = () => {
-  return null;
+  return (
+    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24">
+      <div className="container">
+        <h2 className="section-title">sign up for free today</h2>
+        <p className="section-description mt-5">
+          Celebrate the joy of accomplishment with an app designed to track your
+          progress and motivate your efforts
+        </p>
+        <div className="flex gap-2 mt-10 justify-center">
+          <button className="btn btn-primary">Get for free</button>
+          <button className="btn btn-text">
+            Learn more
+            <ArrowRight />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 };
