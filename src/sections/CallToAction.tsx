e34d5472c3bda7403908a212152029c1915b6432
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-01-07 15:11:41 +0530

    style(CallToAction.tsx): improve layout and styling of CallToAction section by adding images and adjusting positioning for better visual appeal

--- src/sections/CallToAction.tsx
+++ src/sections/CallToAction.tsx
@@ -1,18 +1,36 @@
 import ArrowRight from "@/assets/arrow-right.svg";
+import starImage from "@/assets/star.png";
+import springImage from "@/assets/spring.png";
+import Image from "next/image";
 export const CallToAction = () => {
   return (
-    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24">
+    <section className="bg-gradient-to-b from-white to-[#D2DCFF] py-24 overflow-x-clip">
       <div className="container">
-        <h2 className="section-title">sign up for free today</h2>
-        <p className="section-description mt-5">
-          Celebrate the joy of accomplishment with an app designed to track your
-          progress and motivate your efforts
-        </p>
+        <div className="section-heading relative">
+          <h2 className="section-title">sign up for free today</h2>
+          <p className="section-description mt-5">
+            Celebrate the joy of accomplishment with an app designed to track
+            your progress and motivate your efforts
+          </p>
+          <Image
+            src={starImage}
+            alt="star"
+            className="absolute -left-[350px] -top-[137px]"
+            width={360}
+          />
+          <Image
+            src={springImage}
+            alt="spring"
+            className="absolute -right-[331px] -top-[19px]"
+            width={360}
+          />
+          ;
+        </div>
         <div className="flex gap-2 mt-10 justify-center">
           <button className="btn btn-primary">Get for free</button>
-          <button className="btn btn-text">
-            Learn more
-            <ArrowRight />
+          <button className="btn btn-text gap-1">
+            <span>Learn more</span>
+            <ArrowRight className="h-5 w-5" />
           </button>
         </div>
       </div>
