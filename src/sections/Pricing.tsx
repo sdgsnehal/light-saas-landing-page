0a1b7f48f12dae144dcd058faa829356bad882e7
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-01-08 14:30:41 +0530

    style(Pricing.tsx): add framer-motion library import for motion component style(Pricing.tsx): replace span element with motion.span for animation effect style(Pricing.tsx): update background gradient and animation properties for motion.span element style(ProductShowcase.tsx): add framer-motion library imports for motion, useScroll, useTransform style(ProductShowcase.tsx): add useRef import from react style(ProductShowcase.tsx): add useRef hook to create a reference for section element style(ProductShowcase.tsx): add useScroll hook to get scroll progress and set offset style(ProductShowcase.tsx): add useTransform hook to transform scroll progress into translateY value style(ProductShowcase.tsx): replace Image components with motion.img for animation effect on pyramid and tube images

--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,5 +1,7 @@
+"use client";
 import CheckIcon from "@/assets/check.svg";
 import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion";
 const pricingTiers = [
   {
     title: "Free",
@@ -91,14 +93,23 @@ export const Pricing = () => {
                   </h3>
                   {popular === true && (
                     <div className="inline-flex text-sm px-4 py-1.5 rounded-xl  border border-white/20">
-                      <span
+                      <motion.span
+                        animate={{
+                          backgroundPositionX: "-100%",
+                        }}
+                        transition={{
+                          duration: 1,
+                          repeat: Infinity,
+                          ease: "linear",
+                          repeatType: "loop",
+                        }}
                         className={twMerge(
-                          "bg-linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#3BFFFF,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] text-transparent bg-clip-text font-medium",
+                          "bg-linear-gradient(to_right,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF,#E1CD86,#BBCB92,#71C2EF,#3BFFFF,#DD7DDF)] [background-size:200%] text-transparent bg-clip-text font-medium",
                           inverse === true && "text-white/60"
                         )}
                       >
                         Popular
-                      </span>
+                      </motion.span>
                     </div>
                   )}
                 </div>
