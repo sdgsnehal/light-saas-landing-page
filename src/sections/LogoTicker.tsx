a8f787033e1781453d3510b0343cda55d51b97b3
snehal gaurkhede <snehal.gaurkhede@closeloop.com>
2025-01-08 11:53:37 +0530

    feat(page.tsx): add Footer component to the Home page for improved navigation and branding feat(Footer.tsx): implement Footer component with company information, navigation links, and social media icons refactor(Hero.tsx): refactor Hero section to include framer-motion animations for visual effects refactor(LogoTicker.tsx): add framer-motion animation to LogoTicker component for a scrolling effect with logo images

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,3 +1,4 @@
+"use client";
 import acmeLogo from "@/assets/logo-acme.png";
 import quantumLogo from "@/assets/logo-quantum.png";
 import echoLogo from "@/assets/logo-echo.png";
@@ -5,19 +6,38 @@ import celestialLogo from "@/assets/logo-celestial.png";
 import pulseLogo from "@/assets/logo-pulse.png";
 import apexLogo from "@/assets/logo-apex.png";
 import Image from "next/image";
+import { motion } from "framer-motion";
 export const LogoTicker = () => {
   return (
     <div className="py-8 md:py-12 bg-white">
       <div className="container">
         <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-          <div className="flex gap-14 flex-none">
+          <motion.div
+            className="flex gap-14 flex-none pr-14"
+            animate={{
+              translateX: "-50%",
+            }}
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              ease: "linear",
+              repeatType: "loop",
+            }}
+          >
             <Image src={acmeLogo} alt="acme logo" />
             <Image src={quantumLogo} alt="quantumLogo logo" />
             <Image src={echoLogo} alt="echoLogo logo" />
             <Image src={celestialLogo} alt="celestialLogo logo" />
             <Image src={pulseLogo} alt="pulseLogo logo" />
             <Image src={apexLogo} alt="apexLogo logo" />
-          </div>
+            {/* second set of logos */}
+            <Image src={acmeLogo} alt="acme logo" />
+            <Image src={quantumLogo} alt="quantumLogo logo" />
+            <Image src={echoLogo} alt="echoLogo logo" />
+            <Image src={celestialLogo} alt="celestialLogo logo" />
+            <Image src={pulseLogo} alt="pulseLogo logo" />
+            <Image src={apexLogo} alt="apexLogo logo" />
+          </motion.div>
         </div>
       </div>
     </div>
